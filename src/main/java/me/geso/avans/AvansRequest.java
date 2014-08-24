2c61c3adab6626140418cff2bc86740b8316b869
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-08-24 09:30:01 +0900

    Added AvansRequest#getHeaderMap. Docs.

--- src/main/java/me/geso/avans/AvansRequest.java
+++ src/main/java/me/geso/avans/AvansRequest.java
@@ -4,12 +4,14 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import javax.servlet.ServletInputStream;
@@ -31,19 +33,19 @@ import lombok.SneakyThrows;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-// TODO port all methods from Plack::Request
 /**
- * Not thread safe.
+ * This class represents HTTP request. The object isn't thread safe. You
+ * shouldn't share this object between threads.
  * 
  * @author tokuhirom
  *
  */
 public class AvansRequest {
-	private HttpServletRequest request;
+	private final HttpServletRequest request;
 	private Map<String, List<FileItem>> uploads;
 	private Map<String, String[]> parameters;
 
-	public AvansRequest(HttpServletRequest request) {
+	public AvansRequest(final HttpServletRequest request) {
 		try {
 			request.setCharacterEncoding(this.getCharacterEncoding());
 		} catch (UnsupportedEncodingException e) {
@@ -52,42 +54,136 @@ public class AvansRequest {
 		this.request = request;
 	}
 
+	/**
+	 * Get PATH_INFO.
+	 * 
+	 * @return
+	 */
 	public String getPathInfo() {
 		return this.request.getPathInfo();
 	}
 
+	/**
+	 * Get header string.
+	 * 
+	 * @param name
+	 * @return
+	 */
 	public String getHeader(String name) {
 		return this.request.getHeader(name);
 	}
 
+	/**
+	 * Get all header values by name.
+	 * 
+	 * @param name
+	 * @return
+	 */
 	public List<String> getHeaders(String name) {
 		return Collections.list(this.request.getHeaders(name));
 	}
 
+	/**
+	 * Get all headers in Map.
+	 * 
+	 * @return
+	 */
+	public Map<String, List<String>> getHeaderMap() {
+		Map<String, List<String>> map = new TreeMap<>();
+		Enumeration<String> headerNames = this.request.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			final String name = headerNames.nextElement();
+			final ArrayList<String> values = Collections.list(this.request
+					.getHeaders(name));
+			map.put(name, values);
+		}
+		return map;
+	}
+
+	/**
+	 * Get CONTENT_LENGTH.
+	 * 
+	 * @return
+	 */
 	public int getContentLength() {
 		return this.request.getContentLength();
 	}
 
+	/**
+	 * Get HTTP_METHOD
+	 * 
+	 * @return
+	 */
 	public String getMethod() {
 		return this.request.getMethod();
 	}
 
+	/**
+	 * Returns the part of this request's URL from the protocol name up to the
+	 * query string in the first line of the HTTP request. The web container
+	 * does not decode this String. For example:
+	 * 
+	 * <table summary="Examples of Returned Values">
+	 * <tr align=left>
+	 * <th>First line of HTTP request</th>
+	 * <th>Returned Value</th>
+	 * <tr>
+	 * <td>POST /some/path.html HTTP/1.1
+	 * <td>
+	 * <td>/some/path.html
+	 * <tr>
+	 * <td>GET http://foo.bar/a.html HTTP/1.0
+	 * <td>
+	 * <td>/a.html
+	 * <tr>
+	 * <td>HEAD /xyz?a=b HTTP/1.1
+	 * <td>
+	 * <td>/xyz
+	 * </table>
+	 *
+	 * <p>
+	 * To reconstruct an URL with a scheme and host, use
+	 * {@link HttpUtils#getRequestURL}.
+	 *
+	 * @return a <code>String</code> containing the part of the URL from the
+	 *         protocol name up to the query string
+	 *
+	 * @see HttpUtils#getRequestURL
+	 */
 	public String getRequestURI() {
 		return this.request.getRequestURI();
 	}
 
+	/**
+	 * Get QUERY_STRING.
+	 * 
+	 * @return
+	 */
 	public String getQueryString() {
 		return this.request.getQueryString();
 	}
 
+	/**
+	 * Get cooies.
+	 * 
+	 * @return
+	 */
 	public Cookie[] getCookies() {
 		return this.request.getCookies();
 	}
 
+	/**
+	 * Get session object.
+	 * 
+	 * @return
+	 */
 	public HttpSession getSession() {
 		return this.request.getSession();
 	}
 
+	/**
+	 * Change session id.
+	 */
 	public void changeSessionId() {
 		this.request.changeSessionId();
 	}
@@ -113,16 +209,21 @@ public class AvansRequest {
 		}
 	}
 
+	/**
+	 * Validate object by bean validation.
+	 * 
+	 * @param o
+	 */
 	public <T> void validate(T o) {
 		ValidatorFactory validatorFactory = Validation
 				.buildDefaultValidatorFactory();
 		Validator validator = validatorFactory.getValidator();
 		Set<ConstraintViolation<T>> violations = validator.validate(o);
 		if (!violations.isEmpty()) {
-			String message = violations.stream().map(e -> {
+			List<String> messages = violations.stream().map(e -> {
 				return e.getPropertyPath() + " " + e.getMessage();
-			}).collect(Collectors.joining(","));
-			throw new AvansValidationException(message);
+			}).collect(Collectors.toList());
+			throw new AvansValidationException(messages);
 		}
 	}
 
@@ -165,7 +266,6 @@ public class AvansRequest {
 		}
 	}
 
-	// TODO This may work. but it's not tested.
 	public Optional<FileItem> getFileItem(String name) {
 		List<FileItem> items = this.getFileItemMap().get(name);
 		if (items == null || items.isEmpty()) {
@@ -174,7 +274,6 @@ public class AvansRequest {
 		return Optional.of(items.get(0));
 	}
 
-	// TODO This may work. but it's not tested.
 	public List<FileItem> getFileItems(String name) {
 		List<FileItem> items = this.getFileItemMap().get(name);
 		if (items == null) {
@@ -182,7 +281,7 @@ public class AvansRequest {
 		}
 		return items;
 	}
-	
+
 	public Map<String, List<FileItem>> getFileItemMap() {
 		this.getParameterMap(); // initialize this.uploads
 		return this.uploads;
@@ -203,10 +302,12 @@ public class AvansRequest {
 					this.uploads = new HashMap<>();
 					for (FileItem fileItem : fileItems) {
 						if (fileItem.isFormField()) {
-							String value = fileItem.getString(this.getCharacterEncoding());
+							String value = fileItem.getString(this
+									.getCharacterEncoding());
 							String[] strings = new String[1];
 							strings[0] = value;
-							this.parameters.put(fileItem.getFieldName(), strings);
+							this.parameters.put(fileItem.getFieldName(),
+									strings);
 						} else {
 							if (this.uploads.containsKey(fileItem
 									.getFieldName())) {
--- src/main/java/me/geso/avans/AvansValidationException.java
+++ src/main/java/me/geso/avans/AvansValidationException.java
@@ -1,8 +1,15 @@
 package me.geso.avans;
 
+import java.util.List;
+
+import lombok.ToString;
+
+@ToString
 public class AvansValidationException extends RuntimeException {
-	AvansValidationException(String message) {
-		super(message);
+	private final List<String> messages;
+
+	AvansValidationException(List<String> messages) {
+		this.messages = messages;
 	}
 
 	private static final long serialVersionUID = 1L;
