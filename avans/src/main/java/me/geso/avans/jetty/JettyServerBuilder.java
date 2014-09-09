43815b7e2aa8185328ebed4f326406c5e7190385
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-09-09 14:35:32 +0900

    Added enableAccessLog() method.

--- avans/src/main/java/me/geso/avans/jetty/JettyServerBuilder.java
+++ avans/src/main/java/me/geso/avans/jetty/JettyServerBuilder.java
@@ -2,8 +2,11 @@ package me.geso.avans.jetty;
 
 import me.geso.avans.AvansServlet;
 import me.geso.avans.Controller;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.Slf4jRequestLog;
+import org.eclipse.jetty.server.handler.RequestLogHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
@@ -16,6 +19,7 @@ public class JettyServerBuilder {
 	private final AvansServlet servlet;
 	private int minThreads = 80;
 	private int maxThreads = 80;
+	private boolean accessLogEnabled;
 
 	public JettyServerBuilder() {
 		this.servlet = new AvansServlet();
@@ -36,13 +40,21 @@ public class JettyServerBuilder {
 		return this;
 	}
 
-	// TODO: add enableAccessLog() method.
+	public JettyServerBuilder enableAccessLog() {
+		this.accessLogEnabled = true;
+		return this;
+	}
 
 	public Server build() {
 		QueuedThreadPool queuedThreadPool = new QueuedThreadPool(
 				this.minThreads, this.maxThreads);
-
 		Server server = new Server(queuedThreadPool);
+		if (this.accessLogEnabled) {
+			RequestLogHandler requestLogHandler = new RequestLogHandler();
+			requestLogHandler.setRequestLog(new Slf4jRequestLog());
+			server.setHandler(requestLogHandler);
+		}
+
 		ServerConnector serverConnector = new ServerConnector(server);
 		serverConnector.setPort(port);
 		server.addConnector(serverConnector);
