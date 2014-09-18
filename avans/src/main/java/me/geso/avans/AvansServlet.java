77b42c5e190f60b2297b618536352879eeb504ff
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-09-19 08:36:06 +0900

    javadoc

--- avans/src/main/java/me/geso/avans/AvansServlet.java
+++ avans/src/main/java/me/geso/avans/AvansServlet.java
@@ -9,27 +9,53 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet for Avans.
+ */
 public class AvansServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private final Dispatcher dispatcher = new Dispatcher();
 
+	/**
+	 * Scan package and register all classes.
+	 * 
+	 * @param packageName
+	 */
 	public void registerPackage(String packageName) {
 		this.dispatcher.registerPackage(packageName);
 	}
 
+	/**
+	 * Scan package and register all classes.
+	 * 
+	 * @param packageName
+	 */
 	public void registerPackage(Package pkg) {
 		this.dispatcher.registerPackage(pkg.getName());
 	}
 
+	/**
+	 * Register paths from class.
+	 * 
+	 * @param packageName
+	 */
 	public void registerClass(Class<? extends Controller> klass) {
 		this.dispatcher.registerClass(klass);
 	}
-	
+
+	/**
+	 * Get dispatcher object.
+	 * 
+	 * @return dispatcher object.
+	 */
 	public Dispatcher getDispatcher() {
 		return dispatcher;
 	}
 
+	/**
+	 * Do service.
+	 */
 	public void service(ServletRequest req, ServletResponse res)
 			throws ServletException, IOException {
 		dispatcher.handler(
