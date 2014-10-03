258ddff0f87cea3b4739200a2ad95fb03ef2277e
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-10-03 09:41:51 +0900

    Support web.xml

--- avans/src/main/java/me/geso/avans/AvansServlet.java
+++ avans/src/main/java/me/geso/avans/AvansServlet.java
@@ -2,6 +2,7 @@ package me.geso.avans;
 
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -16,6 +17,29 @@ public class AvansServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private final Dispatcher dispatcher = new Dispatcher();
+
+	/**
+	 * You can register classes by web.xml. You can write web.xml as
+	 * {@code <init-param><param-name>package</param-name><param-value>pkg1,pkg2</param-value></init-param>}
+	 * or
+	 * {@code <init-param><param-name>class</param-name><param-value>pkg1,pkg2</param-value></init-param>}
+	 */
+	@Override
+	public void init(ServletConfig servletConfig) throws ServletException {
+		final String pkg = servletConfig.getInitParameter("package");
+		if (pkg != null) {
+			for (final String p : pkg.split(",")) {
+				this.registerPackage(p);
+			}
+		}
+
+		final String classCsv = servletConfig.getInitParameter("class");
+		if (classCsv != null) {
+			for (final String className : classCsv.split(",")) {
+				this.registerPackage(className);
+			}
+		}
+	}
 
 	/**
 	 * Scan package and register all classes.
