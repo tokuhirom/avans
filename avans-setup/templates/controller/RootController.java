789cfb3f1cba5b71571bf87cf1cd37eadbb132c0
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-10-05 11:23:18 +0900

    Split templates from generator script

---
+++ avans-setup/templates/controller/RootController.java
@@ -0,0 +1,14 @@
+package <%= @pkg %>.controller;
+
+import me.geso.avans.ControllerBase;
+import me.geso.avans.annotation.GET;
+import me.geso.avans.mustache.MustacheView;
+import me.geso.webscrew.response.WebResponse;
+
+public class RootController extends ControllerBase
+    implements MustacheView {
+    @GET("/")
+    public WebResponse index() {
+        return this.renderMustache("index.mustache", null);
+    }
+}
