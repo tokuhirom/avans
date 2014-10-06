66ac5219f5607fd78943fab27d352a4cea070557
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-10-06 23:38:03 +0900

    Support avans-0.30.1 in avans-setup

--- avans-setup/templates/src/main/java/PACKAGE/controller/RootController.java
+++ avans-setup/templates/src/main/java/PACKAGE/controller/RootController.java
@@ -2,13 +2,22 @@ package <%= @package %>.controller;
 
 import me.geso.avans.ControllerBase;
 import me.geso.avans.annotation.GET;
+import me.geso.avans.mustache.MustacheViewMixin;
 import me.geso.avans.mustache.MustacheView;
 import me.geso.webscrew.response.WebResponse;
 
+import java.io.File;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+
 public class RootController extends ControllerBase
-    implements MustacheView {
+    implements MustacheViewMixin {
     @GET("/")
     public WebResponse index() {
         return this.renderMustache("index.mustache", null);
     }
+
+	public MustacheView getMustacheView() {
+		return new MustacheView(new DefaultMustacheFactory(new File("templates/")));
+	}
 }
