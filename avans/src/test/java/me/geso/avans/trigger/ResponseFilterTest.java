ce06cad6ac08ff6ebdf1da0a72a0a704432a42a7
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-11-11 06:13:31 +0900

    Added test case for ResponseFilter

---
+++ avans/src/test/java/me/geso/avans/trigger/ResponseFilterTest.java
@@ -0,0 +1,50 @@
+package me.geso.avans.trigger;
+
+import static org.junit.Assert.assertEquals;
+import me.geso.avans.AvansServlet;
+import me.geso.avans.ControllerBase;
+import me.geso.avans.annotation.GET;
+import me.geso.mech2.Mech2;
+import me.geso.mech2.Mech2Result;
+import me.geso.mech2.Mech2WithBase;
+import me.geso.servlettester.jetty.JettyServletTester;
+import me.geso.webscrew.response.WebResponse;
+
+import org.junit.Test;
+
+public class ResponseFilterTest {
+	public static class MyController extends ControllerBase {
+		@ResponseFilter
+		public void responseFilter(WebResponse resp) {
+			resp.addHeader("x-content-type-options", "nosniff");
+		}
+
+		@GET("/")
+		public WebResponse call() {
+			return this.renderJSON("HOGE");
+		}
+	}
+
+	@Test
+	public void test() throws Exception {
+		final AvansServlet servlet = new AvansServlet();
+		servlet.registerClass(MyController.class);
+		JettyServletTester.runServlet(
+				servlet,
+				(uri) -> {
+					final Mech2WithBase mech2 = new Mech2WithBase(Mech2
+							.builder()
+							.build(), uri);
+					final Mech2Result res = mech2.get("/").execute();
+					assertEquals(200, res.getResponse().getStatusLine()
+							.getStatusCode());
+					assertEquals(
+							"nosniff",
+							res.getResponse()
+									.getFirstHeader("X-Content-Type-Options")
+									.getValue());
+					assertEquals("\"HOGE\"", res.getResponseBodyAsString());
+				});
+	}
+
+}
