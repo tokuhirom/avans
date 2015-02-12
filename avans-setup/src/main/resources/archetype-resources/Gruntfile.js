34a3126cefc62c986b67c9d1ffa3914c3ff0c52d
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-02-13 07:53:56 +0900

    Switch to grunt.

---
+++ avans-setup/src/main/resources/archetype-resources/Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    bower: {
+      install: {
+        options: {
+          targetDir: './src/main/webapp/components/',
+          layout: 'byComponent',
+          install: true,
+          verbose: false,
+          cleanTargetDir: true,
+          cleanBowerDir: true
+        }
+      }
+    },
+  });
+  grunt.loadNpmTasks('grunt-bower-task');
+  grunt.registerTask('default', ['bower:install']);
+};
