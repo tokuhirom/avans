c8566c140f29e1cecfa48f85a5417d075c19b0e3
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-02-19 13:20:04 +0900

    Added ControllerBase#redirect(path, params).

--- avans/src/main/java/me/geso/avans/ControllerBase.java
+++ avans/src/main/java/me/geso/avans/ControllerBase.java
@@ -1,5 +1,9 @@
 package me.geso.avans;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
@@ -10,6 +14,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -23,11 +28,9 @@ import java.util.OptionalLong;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
+import org.apache.http.client.utils.URIBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
@@ -43,9 +46,6 @@ import me.geso.avans.trigger.ResponseConverter;
 import me.geso.webscrew.response.ByteArrayResponse;
 import me.geso.webscrew.response.RedirectResponse;
 import me.geso.webscrew.response.WebResponse;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * You should create this object per HTTP request.
@@ -95,6 +95,21 @@ public abstract class ControllerBase implements Controller,
 	 */
 	public RedirectResponse redirect(@NonNull final String location) {
 		return new RedirectResponse(location);
+	}
+
+	/**
+	 * Create new redirect response. You can use relative url here.
+	 *
+	 * @param location
+	 * @return
+	 */
+	public RedirectResponse redirect(@NonNull final String location, @NonNull Map<String, String> parameters)
+			throws URISyntaxException {
+		final URIBuilder uriBuilder = new URIBuilder(location);
+		parameters.entrySet().stream().forEach(
+			e -> uriBuilder.setParameter(e.getKey(), e.getValue())
+			);
+		return new RedirectResponse(uriBuilder.build().toString());
 	}
 
 	/**
--- avans/src/test/java/me/geso/avans/ControllerBaseTest.java
+++ avans/src/test/java/me/geso/avans/ControllerBaseTest.java
@@ -4,13 +4,20 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.OptionalInt;
 import java.util.OptionalLong;
 import java.util.stream.Collectors;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
 import me.geso.avans.annotation.GET;
 import me.geso.avans.annotation.Param;
@@ -20,10 +27,7 @@ import me.geso.mech2.Mech2WithBase;
 import me.geso.servlettester.jetty.JettyServletTester;
 import me.geso.webscrew.response.WebResponse;
 
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
+import com.google.common.collect.ImmutableMap;
 
 @RunWith(Enclosed.class)
 public class ControllerBaseTest {
@@ -114,15 +118,15 @@ public class ControllerBaseTest {
 			final Filters filters = controller.getFilters();
 			filters.getResponseFilters().forEach(it -> System.out.println(it));
 			final String methods = filters.getResponseFilters().stream()
-					.map(it -> it.getName()).collect(Collectors.joining(","));
+				.map(it -> it.getName()).collect(Collectors.joining(","));
 			System.out.println(methods);
 			assertThat(filters.getResponseFilters(), is(Arrays.asList(
-					this.method(MixinA.class, "filterA"),
-					this.method(MixinA.class, "filterA2"),
-					this.method(ControllerX.class, "filterA2"),
-					this.method(MixinB.class, "filterB"),
-					this.method(ControllerY.class, "filterA")
-					)));
+				this.method(MixinA.class, "filterA"),
+				this.method(MixinA.class, "filterA2"),
+				this.method(ControllerX.class, "filterA2"),
+				this.method(MixinB.class, "filterB"),
+				this.method(ControllerY.class, "filterA")
+				)));
 		}
 	}
 
@@ -148,31 +152,31 @@ public class ControllerBaseTest {
 			// without string parameter
 			JettyServletTester.runServlet(servlet, baseURI -> {
 				final Mech2WithBase mech2 = new Mech2WithBase(Mech2.builder()
-						.build(), baseURI);
+					.build(), baseURI);
 				assertEquals("q=5963", mech2.get("/").execute()
-						.getResponseBodyAsString());
+					.getResponseBodyAsString());
 			});
 			// with empty string
 			JettyServletTester.runServlet(servlet, baseURI -> {
 				final Mech2WithBase mech2 = new Mech2WithBase(Mech2.builder()
-						.build(), baseURI);
+					.build(), baseURI);
 				assertEquals("q=5963", mech2.get("/")
-						.addQueryParameter("q", "")
-						.execute()
-						.getResponseBodyAsString());
+					.addQueryParameter("q", "")
+					.execute()
+					.getResponseBodyAsString());
 			});
 			// with string
 			JettyServletTester.runServlet(
-					servlet,
-					baseURI -> {
-						final Mech2WithBase mech2 = new Mech2WithBase(Mech2
-								.builder()
-								.build(), baseURI);
-						assertEquals("q=4649", mech2.get("/")
-								.addQueryParameter("q", "4649")
-								.execute()
-								.getResponseBodyAsString());
-					});
+				servlet,
+				baseURI -> {
+					final Mech2WithBase mech2 = new Mech2WithBase(Mech2
+						.builder()
+						.build(), baseURI);
+					assertEquals("q=4649", mech2.get("/")
+						.addQueryParameter("q", "4649")
+						.execute()
+						.getResponseBodyAsString());
+				});
 		}
 	}
 
@@ -194,31 +198,31 @@ public class ControllerBaseTest {
 			// without string parameter
 			JettyServletTester.runServlet(servlet, baseURI -> {
 				final Mech2WithBase mech2 = new Mech2WithBase(Mech2.builder()
-						.build(), baseURI);
+					.build(), baseURI);
 				assertEquals("q=5963", mech2.get("/").execute()
-						.getResponseBodyAsString());
+					.getResponseBodyAsString());
 			});
 			// with empty string
 			JettyServletTester.runServlet(servlet, baseURI -> {
 				final Mech2WithBase mech2 = new Mech2WithBase(Mech2.builder()
-						.build(), baseURI);
+					.build(), baseURI);
 				assertEquals("q=5963", mech2.get("/")
-						.addQueryParameter("q", "")
-						.execute()
-						.getResponseBodyAsString());
+					.addQueryParameter("q", "")
+					.execute()
+					.getResponseBodyAsString());
 			});
 			// with string
 			JettyServletTester.runServlet(
-					servlet,
-					baseURI -> {
-						final Mech2WithBase mech2 = new Mech2WithBase(Mech2
-								.builder()
-								.build(), baseURI);
-						assertEquals("q=4649", mech2.get("/")
-								.addQueryParameter("q", "4649")
-								.execute()
-								.getResponseBodyAsString());
-					});
+				servlet,
+				baseURI -> {
+					final Mech2WithBase mech2 = new Mech2WithBase(Mech2
+						.builder()
+						.build(), baseURI);
+					assertEquals("q=4649", mech2.get("/")
+						.addQueryParameter("q", "4649")
+						.execute()
+						.getResponseBodyAsString());
+				});
 		}
 	}
 
@@ -240,31 +244,31 @@ public class ControllerBaseTest {
 			// without string parameter
 			JettyServletTester.runServlet(servlet, baseURI -> {
 				final Mech2WithBase mech2 = new Mech2WithBase(Mech2.builder()
-						.build(), baseURI);
+					.build(), baseURI);
 				assertEquals("q=3.14", mech2.get("/").execute()
-						.getResponseBodyAsString());
+					.getResponseBodyAsString());
 			});
 			// with empty string
 			JettyServletTester.runServlet(servlet, baseURI -> {
 				final Mech2WithBase mech2 = new Mech2WithBase(Mech2.builder()
-						.build(), baseURI);
+					.build(), baseURI);
 				assertEquals("q=3.14", mech2.get("/")
-						.addQueryParameter("q", "")
-						.execute()
-						.getResponseBodyAsString());
+					.addQueryParameter("q", "")
+					.execute()
+					.getResponseBodyAsString());
 			});
 			// with string
 			JettyServletTester.runServlet(
-					servlet,
-					baseURI -> {
-						final Mech2WithBase mech2 = new Mech2WithBase(Mech2
-								.builder()
-								.build(), baseURI);
-						assertEquals("q=2.71", mech2.get("/")
-								.addQueryParameter("q", "2.71")
-								.execute()
-								.getResponseBodyAsString());
-					});
+				servlet,
+				baseURI -> {
+					final Mech2WithBase mech2 = new Mech2WithBase(Mech2
+						.builder()
+						.build(), baseURI);
+					assertEquals("q=2.71", mech2.get("/")
+						.addQueryParameter("q", "2.71")
+						.execute()
+						.getResponseBodyAsString());
+				});
 		}
 	}
 
@@ -285,30 +289,116 @@ public class ControllerBaseTest {
 			servlet.registerClass(Controller.class);
 			JettyServletTester.runServlet(servlet, baseURI -> {
 				final Mech2WithBase mech2 = new Mech2WithBase(Mech2.builder()
-						.build(), baseURI);
+					.build(), baseURI);
 				assertEquals("q=Missing", mech2.get("/").execute()
-						.getResponseBodyAsString());
+					.getResponseBodyAsString());
 			});
 			// with empty string
 			JettyServletTester.runServlet(servlet, baseURI -> {
 				final Mech2WithBase mech2 = new Mech2WithBase(Mech2.builder()
-						.build(), baseURI);
+					.build(), baseURI);
 				assertEquals("q=Missing", mech2.get("/")
-						.addQueryParameter("q", "")
-						.execute()
-						.getResponseBodyAsString());
+					.addQueryParameter("q", "")
+					.execute()
+					.getResponseBodyAsString());
 			});
 			JettyServletTester.runServlet(
-					servlet,
-					baseURI -> {
-						final Mech2WithBase mech2 = new Mech2WithBase(Mech2
-								.builder()
-								.build(), baseURI);
-						assertEquals("q=4649", mech2.get("/")
-								.addQueryParameter("q", "4649")
-								.execute()
-								.getResponseBodyAsString());
-					});
+				servlet,
+				baseURI -> {
+					final Mech2WithBase mech2 = new Mech2WithBase(Mech2
+						.builder()
+						.build(), baseURI);
+					assertEquals("q=4649", mech2.get("/")
+						.addQueryParameter("q", "4649")
+						.execute()
+						.getResponseBodyAsString());
+				});
+		}
+	}
+
+	public static class TestRedirect {
+		public static class Controller extends ControllerBase {
+			@GET("/")
+			public WebResponse root() {
+				return this.redirect("/xxx/yyy?foo=bar");
+			}
+
+			@GET("/xxx/yyy")
+			public WebResponse yyy() {
+				return this.renderText("OK");
+			}
+		}
+
+		@Test
+		public void testX() throws Exception {
+			final AvansServlet servlet = new AvansServlet();
+			servlet.registerClass(Controller.class);
+			JettyServletTester.runServlet(servlet, baseURI -> {
+				final Mech2WithBase mech2 = new Mech2WithBase(Mech2.builder()
+					.build(), baseURI);
+				assertEquals("OK", mech2.get("/").execute()
+					.getResponseBodyAsString());
+			});
+		}
+	}
+
+	public static class TestRedirectWithQueryParameters {
+		public static class Controller extends ControllerBase {
+			@GET("/a")
+			public WebResponse a() throws IOException, URISyntaxException {
+				return this.redirect("/ok", ImmutableMap.<String, String>builder()
+					.put("a", "1")
+					.build());
+			}
+
+			@GET("/b")
+			public WebResponse b() throws Exception {
+				return this.redirect("/ok", ImmutableMap.<String, String>builder()
+					.put("a", "1")
+					.put("b", "2")
+					.build());
+			}
+
+			@GET("/c")
+			public WebResponse c() throws Exception {
+				return this.redirect("/ok", ImmutableMap.<String, String>builder()
+					.put("a", "1")
+					.put("b", "2")
+					.put("c", "3")
+					.build());
+			}
+		}
+
+		@Test
+		public void testX() throws Exception {
+			final AvansServlet servlet = new AvansServlet();
+			servlet.registerClass(Controller.class);
+			JettyServletTester.runServlet(servlet, baseURI -> {
+				// Only one parameter
+				{
+					final Mech2WithBase mech2 = new Mech2WithBase(Mech2.builder()
+						.build(), baseURI);
+					mech2.disableRedirectHandling();
+					assertEquals("/ok?a=1", mech2.get("/a").execute()
+						.getResponse().getFirstHeader("Location"));
+				}
+				// There is two parameters.
+				{
+					final Mech2WithBase mech2 = new Mech2WithBase(Mech2.builder()
+						.build(), baseURI);
+					mech2.disableRedirectHandling();
+					assertEquals("/ok?a=1", mech2.get("/b").execute()
+						.getResponse().getFirstHeader("Location"));
+				}
+				// There is three parameters.
+				{
+					final Mech2WithBase mech2 = new Mech2WithBase(Mech2.builder()
+						.build(), baseURI);
+					mech2.disableRedirectHandling();
+					assertEquals("/ok?a=1&b=2&c=3", mech2.get("/c").execute()
+						.getResponse().getFirstHeader("Location"));
+				}
+			});
 		}
 	}
 
