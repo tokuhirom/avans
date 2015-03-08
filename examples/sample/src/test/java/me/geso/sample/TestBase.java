42979b0764c04760e0c75908c993cc427b1f4997
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-03-08 12:51:10 +0900

    Added basic testing utilities for sample code.

---
+++ examples/sample/src/test/java/me/geso/sample/TestBase.java
@@ -0,0 +1,17 @@
+package me.geso.sample;
+
+import org.junit.BeforeClass;
+
+public class TestBase {
+	@BeforeClass
+	public static void setupClass() {
+		String env = System.getProperty("sample.env");
+		if (env == null) {
+			System.setProperty("sample.env", "test");
+		}
+		env = System.getProperty("sample.env");
+		if (!(env.equals("test"))) {
+			throw new RuntimeException("Do not run test case on non-test environment");
+		}
+	}
+}
