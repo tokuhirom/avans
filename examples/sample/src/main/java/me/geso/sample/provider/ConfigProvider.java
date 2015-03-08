610915fb106636d4dc5a092fe3cf3318bc7245ba
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-03-08 12:53:22 +0900

    Better error detection.

--- examples/sample/src/main/java/me/geso/sample/provider/ConfigProvider.java
+++ examples/sample/src/main/java/me/geso/sample/provider/ConfigProvider.java
@@ -21,16 +21,20 @@ public class ConfigProvider implements Provider<Config> {
 		} else {
 			String fileName = "config/" + env + ".yml";
 			try (InputStream stream = this.getClass().getClassLoader().getResourceAsStream(
-					fileName)) {
+				fileName)) {
+				if (stream == null) {
+					throw new RuntimeException("Cannot load " + fileName
+						+ " from resource.");
+				}
 				try {
 					return new ObjectMapper(new YAMLFactory()).readValue(stream, Config.class);
 				} catch (JsonParseException | JsonMappingException e) {
 					throw new RuntimeException(String.format("Cannot parse %s: %s",
-							fileName, e.getMessage()));
+						fileName, e.getMessage()));
 				}
 			} catch (IOException e) {
 				throw new RuntimeException(String.format("Cannot read %s from resources: %s",
-						fileName, e.getMessage()));
+					fileName, e.getMessage()));
 			}
 		}
 	}
