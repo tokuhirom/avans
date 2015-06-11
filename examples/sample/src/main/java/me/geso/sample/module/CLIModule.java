8d6a76951dc0ea52992e591b2806bae488efce51
Manabu Matsuzaki <matsumana@gmail.com>
2015-06-12 03:05:23 +0900

    In the case of CLI application, SINGLETON is best.

--- examples/sample/src/main/java/me/geso/sample/module/CLIModule.java
+++ examples/sample/src/main/java/me/geso/sample/module/CLIModule.java
@@ -3,6 +3,7 @@ package me.geso.sample.module;
 import java.sql.Connection;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
 
 import me.geso.sample.provider.ConnectionProvider;
 import me.geso.sample.provider.TinyORMProvider;
@@ -12,7 +13,8 @@ public class CLIModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		bind(Connection.class)
-			.toProvider(ConnectionProvider.class);
+			.toProvider(ConnectionProvider.class)
+			.in(Scopes.SINGLETON);
 		bind(TinyORM.class)
 			.toProvider(TinyORMProvider.class);
 	}
