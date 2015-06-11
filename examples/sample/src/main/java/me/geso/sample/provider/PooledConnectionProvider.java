373aaa0cc3abb552484d978e171da9500ecd0536
Manabu Matsuzaki <matsumana@gmail.com>
2015-06-12 02:38:57 +0900

    get the JDBC URL from connectoin metadata

--- examples/sample/src/main/java/me/geso/sample/provider/PooledConnectionProvider.java
+++ examples/sample/src/main/java/me/geso/sample/provider/PooledConnectionProvider.java
@@ -7,8 +7,6 @@ import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.sql.DataSource;
 
-import com.zaxxer.hikari.HikariDataSource;
-
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -26,10 +24,10 @@ public class PooledConnectionProvider implements Provider<Connection> {
 	public Connection get() {
 		try {
 			if (connection == null) {
-				log.debug("Get JDBC connection from connection pool: {}",
-					((HikariDataSource)dataSource).getJdbcUrl());
-
 				connection = dataSource.getConnection();
+
+				log.debug("Get JDBC connection from connection pool: {}",
+					connection.getMetaData().getURL());
 			}
 			return connection;
 		} catch (SQLException e) {
