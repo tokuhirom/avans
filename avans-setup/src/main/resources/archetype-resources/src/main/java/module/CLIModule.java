9fb82f730a81a44e51f00607ed54313ec34759b5
Manabu Matsuzaki <matsumana@gmail.com>
2015-06-12 03:06:43 +0900

    Re-Generate archetype-resources.

--- avans-setup/src/main/resources/archetype-resources/src/main/java/module/CLIModule.java
+++ avans-setup/src/main/resources/archetype-resources/src/main/java/module/CLIModule.java
@@ -6,6 +6,7 @@ package ${package}.module;
 import java.sql.Connection;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
 
 import ${package}.provider.ConnectionProvider;
 import ${package}.provider.TinyORMProvider;
@@ -15,7 +16,8 @@ public class CLIModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		bind(Connection.class)
-			.toProvider(ConnectionProvider.class);
+			.toProvider(ConnectionProvider.class)
+			.in(Scopes.SINGLETON);
 		bind(TinyORM.class)
 			.toProvider(TinyORMProvider.class);
 	}
